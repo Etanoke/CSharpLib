4892b64b816835a489d92963fde51baa5d2e4b02
Etanoke <hogepiyoubuntu@gmail.com>
2016-02-11 19:10:37 +0900

    新規追加

---
+++ MVVMBase/DelegateCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMBase
+{
+    class DelegateCommand : ICommand
+    {
+        public Action<object> ExecuteHandler { get; set; }
+        public Func<object, bool> CanExecuteHandler { get; set; }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = this.CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, null);
+            }
+        }
+
+
+        #region ICommand メンバー
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            var handler = this.CanExecuteHandler;
+            if (handler == null)
+            {
+                return true;
+            } else
+            {
+                return handler(parameter);
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            var handler = this.ExecuteHandler;
+            if (handler != null)
+            {
+                handler(parameter);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ MVVMBase/INotifyBase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace MVVMBase
+{
+    class INotifyBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged メンバー
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
